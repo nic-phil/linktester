f9cf3471f4d2cf9776cce4bdaa9677ae12d70d5f
phil <phil@accessidaho.org>
2017-05-31 08:57:07 -0600

    Updated the link path

--- src/main/java/com/nicusa/id/LinkTester.java
+++ src/main/java/com/nicusa/id/LinkTester.java
@@ -7,8 +7,7 @@ import mslinks.*;
  * Created by phil on 5/31/17.
  */
 public class LinkTester {
-    public static void main(String[] args) {
-        public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
             ShellLink sl = ShellLink.createLink("%COMSPEC% /C javaws https://otc.cdc.nicusa.com/downloads/SwiperApp/client_devices.jnlp")
                     .setWorkingDir("..")
                     .setIconLocation("%SystemRoot%\\system32\\SHELL32.dll");
@@ -18,9 +17,14 @@ public class LinkTester {
                     .setFontSize(24)
                     .setTextColor(5);
 
-            sl.saveTo("testlink.lnk");
+            try {
+                System.out.println("Printing link");
+                sl.saveTo("testlink.lnk");
+            } catch (Exception e) {
+                System.out.println("Exception");
+            }
+
             System.out.println(sl.getWorkingDir());
             System.out.println(sl.resolveTarget());
-        }
     }
 }
