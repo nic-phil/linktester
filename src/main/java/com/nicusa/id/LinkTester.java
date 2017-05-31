29a2b8cea2612af1abf7bf1d374cca72f3a216ea
phil <phil@accessidaho.org>
2017-05-31 08:52:30 -0600

    Initial commit

---
+++ src/main/java/com/nicusa/id/LinkTester.java
@@ -0,0 +1,26 @@
+package com.nicusa.id;
+
+import java.io.IOException;
+import mslinks.*;
+
+/**
+ * Created by phil on 5/31/17.
+ */
+public class LinkTester {
+    public static void main(String[] args) {
+        public static void main(String[] args) throws IOException {
+            ShellLink sl = ShellLink.createLink("%COMSPEC% /C javaws https://otc.cdc.nicusa.com/downloads/SwiperApp/client_devices.jnlp")
+                    .setWorkingDir("..")
+                    .setIconLocation("%SystemRoot%\\system32\\SHELL32.dll");
+            sl.getHeader().setIconIndex(128);
+            sl.getConsoleData()
+                    .setFont(mslinks.extra.ConsoleData.Font.Consolas)
+                    .setFontSize(24)
+                    .setTextColor(5);
+
+            sl.saveTo("testlink.lnk");
+            System.out.println(sl.getWorkingDir());
+            System.out.println(sl.resolveTarget());
+        }
+    }
+}
